d4614b4c0473e699314b555c491e62efe1e274d8
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-07-30 16:27:29 +0100

    Update arrow.py

--- weapons/arrow.py
+++ weapons/arrow.py
@@ -3,11 +3,9 @@ from ursina import *
 class Arrow(Entity):
     def __init__(self):
         super().__init__(
-            model = "body",
-            color = "#BA8C63",
-            scale = (0.7, 0.7, 0.7),
             tag = "arrow"
         )
 
-        self.tip = Entity(model = "tip", parent = self, color = color.gray),
+        self.body = Entity(model = "body", parent = self, color = "#BA8C63")
+        self.tip = Entity(model = "tip", parent = self, color = color.gray)
         self.feather = Entity(model = "feather", parent = self, color = "#CC0A00")
