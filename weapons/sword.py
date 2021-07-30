68fc32fa2e38bcda19f40440530036ae1f9aa102
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-07-30 16:28:25 +0100

    you can now pick up and drop your sword

--- weapons/sword.py
+++ weapons/sword.py
@@ -1,14 +1,43 @@
 from ursina import *
-from ursina.shaders import ssao_shader
 
 class Sword(Entity):
-    def __init__(self, position = (2, 0, 2.5), rotation = (0, 90, 0), parent = camera):
+    def __init__(self, rotation = (0, 90, 0), parent = scene):
         super().__init__(
             model = "sword.obj",
             parent = parent,
-            position = position,
+            position = (-971, 50, 34),
             rotation = rotation,
-            shader = ssao_shader,
             tag = "sword",
             texture = "sword.png"
         )
+
+        self.equipped = False
+        self.player = None
+        self.bow = None
+        self.arrow = None
+
+    def update(self):
+        if held_keys["e"] and distance(self.player, self) <= 10:
+            self.equipped = True
+            self.parent = camera
+            self.position = (2, 0, 2.5)
+
+            if self.bow.equipped == True:
+                self.bow.disable()
+                self.arrow.disable()
+        
+        if held_keys["q"] and self.equipped == True:
+            self.equipped = False
+            self.parent = scene
+            self.position = self.player.position
+            self.y = self.player.y + 1
+
+        ray = raycast(self.position, self.down, distance = 1.5, ignore = [self, self.player, ])
+
+        if self.equipped == False and not ray.hit:
+            self.y -= 1 * 9.81 * time.dt 
+
+        if self.equipped == True:
+            self.always_on_top = True
+        else:
+            self.always_on_top = False
